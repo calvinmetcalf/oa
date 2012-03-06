851060b6e3fdd56414429851da3d6a4195d0d7b2
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-03-06 13:32:32 +0000

    'we now have the ability to do url querrys'

--- map.js
+++ map.js
@@ -1,13 +1,23 @@
- var m;
- var center = new google.maps.LatLng(42.04113400940814, -71.795654296875);
- var zoom = 8;
+var m;
 var tableid = 1632887;
+
+function initialize() {
+    var hash = document.location.hash.substring(1);
+    var args = {};
+    var pairs = hash.split("&");
  
- function initialize() {
+    for(var i = 0; i < pairs.length; i++){
+        var pos = pairs[i].indexOf('=');
+        if (pos == -1) continue;
+        var name = pairs[i].substring(0,pos);
+        var value = pairs[i].substring(pos+1);
+        value = decodeURIComponent(value);
+        args[name] = value;
+        };
 
   m = new google.maps.Map(document.getElementById('map'), {
-      center: center,
-      zoom: zoom,
+      center: new google.maps.LatLng(args.lat,args.lng),
+      zoom: +args.zoom,
       mapTypeId: 'roadmap'
     });
  
