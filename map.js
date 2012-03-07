40c81a8ed6b919a9accae4887798f1e0657f915d
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-03-07 13:34:30 +0000

    'can query, still not as DRY as id like though'

--- map.js
+++ map.js
@@ -5,31 +5,24 @@ var geocoder = new google.maps.Geocoder();
 var zoom = 8;
 var center = new google.maps.LatLng(42.04113400940814,-71.795654296875);
 var marker;
+var mainLayer
 
 function initialize() {
- 
-
+    
   m = new google.maps.Map(document.getElementById('map'), {
       center: center,
       zoom: zoom,
       mapTypeId: 'roadmap'
     });
- 
-  
-    
-    
-    
+
 var baseLayer = new google.maps.FusionTablesLayer(baseid);
   baseLayer.setQuery("SELECT 'geometry' FROM " + baseid);
   baseLayer.setMap(m);
   
-var mainLayer = new google.maps.FusionTablesLayer(oaid);
+ mainLayer = new google.maps.FusionTablesLayer(oaid);
   mainLayer.setQuery("SELECT 'point' FROM " + oaid + " WHERE 'Status' = 'Active'");
   mainLayer.setMap(m);
-  
   }
-  
-
 
 function geocode() {
      var address = document.getElementById("address").value;
@@ -45,7 +38,6 @@ function geocode() {
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
-    
 }
 
 function resetgeo() {
@@ -53,4 +45,80 @@ function resetgeo() {
     m.setCenter(center);
     m.setZoom(zoom);
 marker.setMap(null);
+}
+
+ $(function() {
+    	$( "#tabs" ).tabs({
+			collapsible: true,
+            selected: -1
+		});
+        $( "input:submit,input:reset" ).button();
+	});
+    
+    google.load('visualization', '1', {});
+    
+function popLists(){    
+    var querySignText = encodeURIComponent("SELECT 'SignType', COUNT() FROM " + oaid + " GROUP BY 'SignType'");
+    var querySign = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + querySignText);
+	querySign.send(getSignData);
+    
+    var queryHolderText = encodeURIComponent("SELECT 'PermitHoldersName', COUNT() FROM " + oaid + " GROUP BY 'PermitHoldersName'");
+    var queryHolder = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryHolderText);
+	queryHolder.send(getHolderData);
+            
+    var queryMuniText = encodeURIComponent("SELECT 'SignCity', COUNT() FROM " + oaid + " GROUP BY 'SignCity'");
+	var queryMuni = new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryMuniText);
+	queryMuni.send(getMuniData);
+}
+
+function getSignData(response) {
+  
+  // Get the number of rows
+  numRows = response.getDataTable().getNumberOfRows();
+  
+  // Add options to the select menu based on the results
+  typeSelect = document.getElementById("signType");  
+  for(i = 0; i < numRows; i++) {
+      newoption = document.createElement('option');
+  	newoption.setAttribute('value'," AND 'SignType' like '" + response.getDataTable().getValue(i, 0) + "'");
+  	newoption.innerHTML = response.getDataTable().getValue(i, 0);
+  	typeSelect.appendChild(newoption);
+  }  
+}
+
+function getHolderData(response) {
+  
+  // Get the number of rows
+  numRows = response.getDataTable().getNumberOfRows();
+  
+  // Add options to the select menu based on the results
+  holderSelect = document.getElementById("holderName");  
+  for(i = 0; i < numRows; i++) {
+      newoption = document.createElement('option');
+ 	newoption.setAttribute('value'," AND 'PermitHoldersName' like '" + response.getDataTable().getValue(i, 0) + "'");
+  	newoption.innerHTML = response.getDataTable().getValue(i, 0);
+  	holderSelect.appendChild(newoption);
+  }  
+}
+function getMuniData(response) {
+  
+  // Get the number of rows
+  numRows = response.getDataTable().getNumberOfRows();
+  
+  // Add options to the select menu based on the results
+  muniSelect = document.getElementById("cityTown");  
+  for(i = 0; i < numRows; i++) {
+  	newoption = document.createElement('option');
+  	newoption.setAttribute('value'," AND 'SignCity' like '" + response.getDataTable().getValue(i, 0) + "'");
+  	newoption.innerHTML = response.getDataTable().getValue(i, 0);
+  	muniSelect.appendChild(newoption);
+  }  
+}
+
+function changeMap() {
+  var signType = document.getElementById('signType').value.replace("'", "\\'");
+  var holderName = document.getElementById('holderName').value.replace("'", "\\'");
+  var cityTown = document.getElementById('cityTown').value.replace("'", "\\'");
+  mainLayer.setQuery("SELECT 'point' FROM " + oaid + " WHERE 'Status' = 'Active' " + signType + holderName + cityTown);
+ 
 }
